58954884e8218781bb380db9e2751dc519b4134d
Igor <mrronin@yandex.ru>
2018-02-16 12:08:16 +0300

    Chat bubbles

---
+++ MyApp/View/SenderChatTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ChatTableViewCell.swift
+//  MyApp
+//
+//  Created by Ronin on 16/02/2018.
+//  Copyright © 2018 Ronin. All rights reserved.
+//
+
+import UIKit
+
+class SenderChatTableViewCell: UITableViewCell {
+    @IBOutlet var messageTextView: UILabel!
+    static let reuseIdentifier = "ChatTableViewCell"
+}
